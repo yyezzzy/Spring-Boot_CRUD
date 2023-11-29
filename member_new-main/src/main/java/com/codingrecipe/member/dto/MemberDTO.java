a8b977fc3fdf64a03237a349e18552c3d71e8408
yyezzzy <t470747877@gmail.com>
2023-11-29 10:58:05 +0900

    first commit

---
+++ member_new-main/src/main/java/com/codingrecipe/member/dto/MemberDTO.java
@@ -0,0 +1,27 @@
+package com.codingrecipe.member.dto;
+
+import com.codingrecipe.member.entity.MemberEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberDTO {
+    private Long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+    private String roadAddr;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        memberDTO.setRoadAddr(memberEntity.getRoadAddr());
+        return memberDTO;
+    }
+}
